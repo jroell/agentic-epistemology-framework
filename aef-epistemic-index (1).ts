695711778711a3a698778698a5f3a0417d5413d7
jroell <roelljr@gmail.com>
2025-04-11 12:31:04 -0400

    cleanup

--- aef-epistemic-index (1).ts
+++
@@ -1,8 +0,0 @@
-/**
- * Re-export all epistemic components from the epistemic directory
- */
-
-export * from './belief';
-export * from './justification';
-export * from './frame';
-export * from './conflict';
